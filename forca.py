c878658b46ce8aad1a117f0c85be89051017813d
Lucas Fernando Alves Ferreira <37386521+lfaferreira@users.noreply.github.com>
2022-02-20 16:40:49 -0300

    Cria prototipo de verificação das letras tentadas pelo usuario.

--- forca.py
+++ forca.py
@@ -6,14 +6,23 @@ def jogar():
     print('Bem vindo ao jogo de Forca!')
     print(separacao)
 
-    print('Fim do Jogo!')
 
-    palavra_secreta = 'Banana'
+    palavra_secreta = 'banana'
     acertou = False
     enforcado = False
 
     while not acertou and not enforcado:
+
+        chute = input('Qual a letra? ').lower().strip()
+
+        index = 1
+        for letra in palavra_secreta:
+            if chute == letra:
+                print(f'Encontrei a letra {letra} na posição: {index}')
+            index += 1
         print('Jogando...')
 
-if (__name__ == '__main__'):
+print('\nFim do Jogo!')
+
+if __name__ == '__main__':
     jogar()
