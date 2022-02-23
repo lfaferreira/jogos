2e5ef8a413d07df9e58e16e2e42c85f1892271b9
Lucas Fernando Alves Ferreira <37386521+lfaferreira@users.noreply.github.com>
2022-02-23 02:12:43 -0300

    Adiciona quantidade de tentativas, respota para vitoria ou derrota e melhora apresentação do jogo em execução

--- forca.py
+++ forca.py
@@ -7,26 +7,43 @@ def jogar():
 
 
     palavra_secreta = 'banana'
-    letras_acertadas = ['_', '_', '_', '_', '_', '_']
+    letras_acertadas = ['_' for letra in palavra_secreta]
+
     acertou = False
     enforcado = False
+    tentativas = 0
 
-    print(f'\n{letras_acertadas}')
+    print(f'\n{letras_acertadas}'
+          f'\n')
 
     while not acertou and not enforcado:
 
         chute = input('Qual a letra? ').lower().strip()
 
-        index = 0
-        for letra in palavra_secreta:
-            if chute == letra:
-                letras_acertadas[index] = letra
-            index += 1
-        print(letras_acertadas)
-        letras_faltando = str(letras_acertadas.count('_'))
-        print(f'Ainda falta acetar {letras_faltando}')
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            tentativas += 1
+            print(f'Eita, errase! Falta {6 - tentativas} tentativas.')
 
-print('\nFim do Jogo!')
+        print(f'{letras_acertadas}')
+        letras_faltando = str(letras_acertadas.count('_'))
+        print(f'Ainda falta acetar {letras_faltando}'
+              f'\n')
+
+        if tentativas == 6 or '_' not in letras_acertadas:
+            break
+
+    if '_' not in letras_acertadas:
+        print('Parabéns!!! Você ganhou.')
+    else:
+        print('Você perdeu. Tente novamente!')
+
+    print('Fim do Jogo!')
 
 if __name__ == '__main__':
     jogar()
